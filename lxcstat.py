706d1c6d5f6bb886f6ddc70dfa93ac659c4a8bc1
Alexander Nikonov <sgloom@gmail.com>
2015-04-02 11:45:30 +0500

    Checks interval changed.

--- lxcstat.py
+++ lxcstat.py
@@ -3,20 +3,6 @@
 #
 #  lxcstat.py
 #  
-#  Copyright 2015 Nikonov Alexander <sgloom@gmail.com>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
@@ -27,8 +13,8 @@ from optparse import OptionParser
 from collections import Iterable
 from subprocess import Popen, PIPE
 
-
-interval = 1
+# Interval in seconds between checks.
+interval = 5
 
 def main():
 		
